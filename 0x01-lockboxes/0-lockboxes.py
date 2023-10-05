a2228cda8162681d6d3f969f54b475c081fa389e
Stevetech1 <steveojata54@gmail.com>
2023-10-05 12:35:17 +0000

    alx-interview/0x01-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # Create a set to keep track of opened boxes.
+    opened_boxes = set()
+    # Start with the first box open.
+    opened_boxes.add(0)
+    # Create a stack to perform DFS.
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        keys = boxes[current_box]
+
+        # Check each key in the current box.
+        for key in keys:
+            if key < len(boxes) and key not in opened_boxes:
+                # If the key corresponds to a valid box and that box is not opened, open it.
+                opened_boxes.add(key)
+                stack.append(key)
+
+    # Check if all boxes have been opened.
+    return len(opened_boxes) == len(boxes)
+
